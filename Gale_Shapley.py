ff3fb99671d739ffdd88aafc8147435aca1be0f7
willzoo <zhujwill@gmail.com>
2026-01-30 21:49:02 -0500

    Fix last bugs

--- Gale_Shapley.py
+++ Gale_Shapley.py
@@ -48,7 +48,7 @@ def verification(length, hospital_rank, student_rank, assignment_of_h, assignmen
                 if student_verify[student][hospital] < student_verify[student][assignment_of_s[student]]:
                     # If a hospital prefers a student over its current assignment and the student does too,
                         # Print blocking pair, make stable false, and break to save time
-                    print(f"UNSTABLE: blocking pair (hospital {hospital + 1}, student {student + 1}")
+                    print(f"UNSTABLE: blocking pair (hospital {hospital + 1}, student {student + 1})")
                     stable = False
                     return f"UNSTABLE: blocking pair (hospital {hospital + 1}, student {student + 1}"
         if not stable:
@@ -112,8 +112,8 @@ if __name__ == "__main__":
                 temp_student.append(read_num(file))
             student_ranks.append(temp_student)
 
-        hospital_copy = hospital_ranks.copy()
-        student_copy = student_ranks.copy()
+        hospital_copy = [row[:] for row in hospital_ranks]
+        student_copy = [row[:] for row in student_ranks]
     
     assignment_of_h, assignment_of_s = gale_shapley(hospital_ranks, student_ranks, length)    
 
